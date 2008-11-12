e0cdae6b44e57764bbdc2a45ec2cb3e237bcb87f
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2008-11-12 16:53:13 +0000

    Removed file/folder

--- branches/1.4.x/sql/migrate/migrate-1.3-to-1.4.sql
+++
@@ -1,3 +0,0 @@
-﻿# add index to cahce table
-
-CREATE INDEX xerxes_cache_grouping_idx ON xerxes_cache(grouping);
--- branches/1.4.x/sql/web-user.sql
+++
@@ -1,2 +0,0 @@
-
-GRANT SELECT, INSERT, DELETE, UPDATE ON xerxes.* TO 'xerxes'@'localhost' IDENTIFIED BY 'password';
