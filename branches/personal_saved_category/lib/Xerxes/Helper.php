c83f4991325cf15ca157cc4f26b56a5230398b3b
rochkind@jhu.edu <rochkind@jhu.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2008-12-11 22:42:17 +0000

    include save_to_collection_url in database XML

--- branches/personal_saved_category/lib/Xerxes/Helper.php
+++ branches/personal_saved_category/lib/Xerxes/Helper.php
@@ -119,10 +119,16 @@ class Xerxes_Helper
 		$objDatabase->appendChild( $objElement );
 		
 		//add an element for url to xerxes detail page for this db
-		
 
 		$objElement = $objDom->createElement( "url", $objRequest->url_for( array ("base" => "databases", "action" => "database", "id" => htmlentities( $objDatabaseData->metalib_id ) ) ) );
 		$objDatabase->appendChild( $objElement );
+    
+    // The 'add to personal collection' url for logged in user, if there
+    // is a logged in user. 
+    if ( Xerxes_Framework_Restrict::isAuthenticatedUser($objRequest) ) {
+      $objElement = $objDom->createElement( "add_to_collection_url", $objRequest->url_for( array ("base" => "collections", "action" => "save_choose_collection", "id" => htmlentities( $objDatabaseData->metalib_id), "username" => $objRequest->getSession("username") ) ) );
+      $objDatabase->appendChild( $objElement );
+    }
 		
 		//add an element for url to xerxes-mediated direct link to db. 
 		
