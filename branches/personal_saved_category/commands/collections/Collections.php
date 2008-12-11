fd68de71bb75324f09be89aaf743fbb60dc00b3d
rochkind@jhu.edu <rochkind@jhu.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2008-12-11 23:00:56 +0000

    that was an incorrect way of generating a full url. This way is what's used in Xerxes generally.

--- branches/personal_saved_category/commands/collections/Collections.php
+++ branches/personal_saved_category/commands/collections/Collections.php
@@ -9,8 +9,15 @@ abstract class Xerxes_Command_Collections extends Xerxes_Framework_Command
 {
 
   public function returnWithMessage($strMessage) {
-    $url = $this->registry->getConfig( 'BASE_WEB_PATH', false, "" ) . "/".  $this->request->getProperty($return);
-
+    $return = $this->request->getProperty("return");
+    
+    if ( $return ) {
+      $url = "http://" . $this->request->getServer('SERVER_NAME') .  $this->request->getProperty("return");
+    }
+    else {
+      $url = $this->registry->getConfig("BASE_WEB_PATH");
+    }
+    
     $this->request->setSession("flash_message", $strMessage);
     $this->request->setRedirect( $url );
   }
