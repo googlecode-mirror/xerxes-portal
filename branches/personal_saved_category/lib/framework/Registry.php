bb6a2d42787c79e5b978bb36f18feff527e3a47f
rochkind@jhu.edu <rochkind@jhu.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2008-12-16 20:17:54 +0000

    Merged revisions 345-370 via svnmerge from https://xerxes-portal.googlecode.com/svn/trunk
    ........
      r350 | dwalker@calstate.edu | 2008-12-15 16:47:01 -0500 (Mon, 15 Dec 2008) | 1 line
      
      mango stuff
    ........
      r352 | dwalker@calstate.edu | 2008-12-15 17:04:57 -0500 (Mon, 15 Dec 2008) | 1 line
      
      missed mango edition
    ........
      r355 | dwalker@calstate.edu | 2008-12-16 11:27:38 -0500 (Tue, 16 Dec 2008) | 1 line
      
      removed file and logic for alternate php config.
    ........
      r366 | dwalker@calstate.edu | 2008-12-16 13:41:36 -0500 (Tue, 16 Dec 2008) | 1 line
      
      removed 'english-only subjects' this never really worked well
    ........

--- branches/personal_saved_category/lib/framework/Registry.php
+++ branches/personal_saved_category/lib/framework/Registry.php
@@ -14,10 +14,8 @@
 class Xerxes_Framework_Registry
 {
 	private $xml = ""; // simple xml object copy
-	private $file = "config/config.xml"; // settings configuration file
-	private $php = "config/config.php"; // settings configuration in a php file
 	private $usergroups = array ( ); // user groups
-  private $authentication_sources = array();
+	private $authentication_sources = array ( );
 	private $arrConfig = null; // configuration settings
 	private $arrPass = array ( ); // values to pass on to the view
 	private static $instance; // singleton pattern
@@ -44,139 +42,96 @@ class Xerxes_Framework_Registry
 	}
 	
 	/**
-	 * Initialize the object by picking up and storing the config xml file
+	 * Initialize the object by picking up and processing the config xml file
 	 * 
 	 * @exception 	will throw exception if no configuration file can be found
 	 */
 	
 	public function init()
 	{
-		// after the initial parsing of the xml file, we'll save
-		// this object in session (?), so make sure we only parse the
-		// file if the configuration array is missing
-		
-
 		if ( $this->arrConfig == null )
 		{
-			$bolUsePHP = false;
+			$file = "";
+			$file_xml = "config/config.xml";
+			$file_php = "config/config.php";
 			
-			$this->arrConfig = array ( );
+			$this->arrConfig = array ();
 			
-			// there is no xml file
+			// check if the config file has an .xml or .php extension
 			
-
-			if ( ! file_exists( $this->file ) )
+			if ( file_exists( $file_xml ) )
 			{
-				// assume we'll use the php file, unless that doesn't exist either,
-				// in which case we need to throw an error
-				
-
-				$bolUsePHP = true;
-				
-				if ( ! file_exists( $this->php ) )
-				{
-					throw new Exception( "could not find configuration file" );
-				}
+				$file = $file_xml;
+			}
+			elseif ( file_exists($file_php) )
+			{
+				$file = $file_php;
+			}
+			else
+			{
+				throw new Exception( "could not find configuration file" );
 			}
 			
-			// use the php file
-			
+			// get it!
 
-			if ( $bolUsePHP == true )
+			$xml = simplexml_load_file( $file );
+			$this->xml = $xml;
+				
+			foreach ( $xml->configuration->config as $config )
 			{
-				require_once ($this->php);
-				
-				$arrPass = Xerxes_Config::$pass;
-				
-				$class = new ReflectionClass( 'Xerxes_Config' );
-				
-				// add it to the config array
-				
-
-				foreach ( $class->getConstants() as $key => $text )
-				{
-					$name = strtoupper( $key );
-					$value = trim( $text );
+				$name = strtoupper( $config["name"] );
+				$value = trim( ( string ) $config );
 					
-					if ( $value != "" )
+				// convert simple xml-encoded values to something easier 
+				// for the client code to digest
+
+				$value = str_replace( "&lt;", "<", $value );
+				$value = str_replace( "&gt;", ">", $value );
+				$value = str_replace( "&amp;", "&", $value );
+					
+				// special logic for authentication_source because we can
+				// have more than one. 
+
+				if ( $name == "AUTHENTICATION_SOURCE" )
+				{
+					$this->authentication_sources[( string ) $config["id"]] = $value;
+
+					// and don't overwrite the first one in our standard config array
+
+					if ( ! empty( $this->arrConfig["AUTHENTICATION_SOURCE"] ) )
 					{
-						//  add it to the config array
-						
-
-						$this->arrConfig[$name] = $value;
-						
-						// entires that are listed in the 'pass' array will be forwarded
-						// on to the xml layer for use in the view
-						
-
-						if ( in_array( $key, $arrPass ) )
-						{
-							$this->arrPass[strtolower( $name )] = $value;
-						}
+						$value = "";
 					}
 				}
-			} else
-			{
-				// use the xml file	
-				
-				$xml = simplexml_load_file( $this->file );
-				$this->xml = $xml;
-				
-				foreach ( $xml->configuration->config as $config )
+					
+				if ( $value != "" )
 				{
-					$name = strtoupper( $config["name"] );
-					$value = trim( ( string ) $config );
-					
-					// convert simple xml-encoded values to something easier 
-					// for the client code to digest
-					
+					// add it to the config array
 
-					$value = str_replace( "&lt;", "<", $value );
-					$value = str_replace( "&gt;", ">", $value );
-					$value = str_replace( "&amp;", "&", $value );
+					$this->arrConfig[$name] = $value;
 					
-          // special logic for authentication_source because we can
-          // have more than one. 
-          if ( $name == "AUTHENTICATION_SOURCE") {
-            $this->authentication_sources[ (string) $config["id"] ] = $value;
-            // And don't overwrite the first one in our standard config array
-            if (! empty($this->arrConfig["AUTHENTICATION_SOURCE"])) {
-              $value = "";
-            }
-          }
-        
-					if ( $value != "" )
+					// types that are listed as 'pass' will be forwarded
+					// on to the xml layer for use in the view
+					
+					if ( ( string ) $config["pass"] == "true" )
 					{
-    
-						// add it to the config array
-
-						$this->arrConfig[$name] = $value;
-						
-						// types that are listed as 'pass' will be forwarded
-						// on to the xml layer for use in the view
-						
-
-						if ( ( string ) $config["pass"] == "true" )
-						{
-							$this->arrPass[strtolower( $name )] = $value;
-						}
+						$this->arrPass[strtolower( $name )] = $value;
 					}
 				}
-        
+			}
 				
-				// get group information out of config.xml too
-				// we just store actual SimpleXML elements in the 
-				// $this->usergroups array.
-
-				$groups = $xml->configuration->groups->group;
+			// get group information out of config.xml too
+			// we just store actual simplexml elements in the 
+			// $this->usergroups array.
 				
-				if ( $groups != false )
+			$groups = $xml->configuration->groups->group;
+				
+			if ( $groups != false )
+			{
+				foreach ( $groups as $group )
 				{
-					foreach ( $groups as $group )
-					{
-						$id = ( string ) $group["id"];
-						$this->usergroups[$id] = $group;
-					}
+					$id = ( string ) $group["id"];
+					$this->usergroups[$id] = $group;
 				}
 			}
 		}
@@ -198,19 +153,23 @@ class Xerxes_Framework_Registry
 		if ( $this->arrConfig == null )
 		{
 			return null;
-		} elseif ( array_key_exists( $name, $this->arrConfig ) )
+		} 
+		elseif ( array_key_exists( $name, $this->arrConfig ) )
 		{
 			if ( $this->arrConfig[$name] == "true" )
 			{
 				return true;
-			} elseif ( $this->arrConfig[$name] == "false" )
+			} 
+			elseif ( $this->arrConfig[$name] == "false" )
 			{
 				return false;
-			} else
+			} 
+			else
 			{
 				return $this->arrConfig[$name];
 			}
-		} else
+		} 
+		else
 		{
 			if ( $bolRequired == true )
 			{
@@ -220,7 +179,8 @@ class Xerxes_Framework_Registry
 			if ( $default != null )
 			{
 				return $default;
-			} else
+			} 
+			else
 			{
 				return null;
 			}
@@ -267,45 +227,13 @@ class Xerxes_Framework_Registry
 		}
 	}
 	
-	/**
-	 * Get a value from the PHP configuration file
-	 *
-	 * @param string $class			class name enclosing the configuration settings
-	 * @param string $key			configuration constant name
-	 * @return mixed				configuration entry value
-	 */
-	
-	private function getHidden($class, $key)
-	{
-		$value = "";
-		$configName = "$class::$key";
-		
-		if ( ! defined( $configName ) )
-			throw new Exception( "php config file lacks entry for $key" );
-		
-		$value = constant( $configName );
-		
-		if ( is_bool( $value ) )
-		{
-			if ( $value == true )
-			{
-				return "true";
-			} else
-			{
-				return "false";
-			}
-		} else
-		{
-			return $value;
-		}
-	}
-	
 	public function userGroups()
 	{
 		if ( $this->usergroups != null )
 		{
 			return array_keys( $this->usergroups );
-		} else
+		} 
+		else
 		{
 			return null;
 		}
@@ -317,7 +245,8 @@ class Xerxes_Framework_Registry
 		{
 			$group = $this->usergroups[$id];
 			return ( string ) $group->display_name;
-		} else
+		} 
+		else
 		{
 			return $id;
 		}
@@ -329,14 +258,15 @@ class Xerxes_Framework_Registry
 		{
 			$group = $this->usergroups[$id];
 			return ( string ) $group->local_ip_range;
-		} else
+		} 
+		else
 		{
 			return $id;
 		}
 	}
 	
 	// returns a simple xml object from the config
-	
+
 	public function getGroupXml($id)
 	{
 		if ( array_key_exists( $id, $this->usergroups ) )
@@ -348,21 +278,32 @@ class Xerxes_Framework_Registry
 			return null;
 		}
 	}
-  
-  // Gets an authentication source by id. If id is null or no such
-  // source can be found, returns first authentication source in config file.
-  // If not even that, returns "demo". 
-  public function getAuthenticationSource($id) {
-    $source = null;
-    if (! empty($id)) $source = $this->authentication_sources[$id];
-    if ($source == null) {
-      $source = $this->getConfig("AUTHENTICATION_SOURCE");
-    }
-    if ($source == null) {
-      $source = "demo";
-    }
-    return $source;
-  }
+	
+	// Gets an authentication source by id. If id is null or no such
+	// source can be found, returns first authentication source in config file.
+	// If not even that, returns "demo".
+	
+	public function getAuthenticationSource($id)
+	{
+		$source = null;
+		
+		if ( ! empty( $id ) )
+		{
+			$source = $this->authentication_sources[$id];
+		}
+		
+		if ( $source == null )
+		{
+			$source = $this->getConfig( "AUTHENTICATION_SOURCE" );
+		}
+		
+		if ( $source == null )
+		{
+			$source = "demo";
+		}
+		
+		return $source;
+	}
 	
 	public function getXML()
 	{
