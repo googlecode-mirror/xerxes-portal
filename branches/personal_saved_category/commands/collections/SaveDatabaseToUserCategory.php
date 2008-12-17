232cd71603a596e858cac8b530d7c8c780b33563
rochkind@jhu.edu <rochkind@jhu.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2008-12-17 15:31:03 +0000

    better feedback messages

--- branches/personal_saved_category/commands/collections/SaveDatabaseToUserCategory.php
+++ branches/personal_saved_category/commands/collections/SaveDatabaseToUserCategory.php
@@ -76,14 +76,14 @@ class Xerxes_Command_SaveDatabaseToUserCategory extends Xerxes_Command_Collectio
     // And add the db to it, unless it already is there. 
     foreach ($subcategory->databases as $db) {
       if ($db->metalib_id == $strDatabaseID) {
-        $this->returnWithMessage("Database was already in personal collection ".  $category->name);
+        $this->returnWithMessage("Database was already in personal collection ".  $category->name . " / " . $subcategory->name);
         return 1;
       }
     }
     $objData->addDatabaseToUserCreatedSubcategory($strDatabaseID, $subcategory);    
     
     // Send them back where they came from, with a message. 
-    $this->returnWithMessage("Stored database in personal collection " . $category->name);
+    $this->returnWithMessage("Stored database in personal collection " . $category->name . " / " . $subcategory->name );
     
 		return 1;
 	}
