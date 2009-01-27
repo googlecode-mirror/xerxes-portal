80976cd2339998d5e4bc088aae95aa7ec5a2957f
rochkind@jhu.edu <rochkind@jhu.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2009-01-27 22:25:45 +0000

    Tighten up access control for privacy; meant to provide for future API expansion, but cross that bridge when we come to it.

--- trunk/commands/collections/ListUserCategories.php
+++ trunk/commands/collections/ListUserCategories.php
@@ -24,17 +24,22 @@
 		public function doExecute( Xerxes_Framework_Request $objRequest, Xerxes_Framework_Registry $objRegistry )
 		{
       // If supplied in URL, use that (for future API use). 
-      $username = $objRequest->getProperty("username");
-      if ( ! $username ) {
+      // Nevermind, this is a privacy problem until we right access
+      // controls for that future API use. 
+      //$username = $objRequest->getProperty("username");
+      //if ( ! $username ) {
         // default to logged in user
         $username = $objRequest->getSession("username");
-      }
+      //}     
       
       // We can only do this if we have a real user (not temp user), otherwise
       // just add no XML. 
       if ($username == null || ! Xerxes_Framework_Restrict::isAuthenticatedUser( $objRequest )) {
           return;
       }
+      
+
+      
       
 			$objXml = new DOMDOcument();
 			
