29a7f12b12c99cf91348e101cd5fb43d84958911
rochkind <rochkind@jhu.edu>
2009-02-17 22:30:01 +0000

    no need for auth check there, and it messes things up if user clicks 'save database' when not YET logged in.

--- commands/collections/ChooseCategorySkip.php
+++ commands/collections/ChooseCategorySkip.php
@@ -25,8 +25,7 @@ class Xerxes_Command_ChooseCategorySkip extends Xerxes_Command_Collections
 		
     $strUsername = $objRequest->getProperty("username");            
     
-    // Make sure they are logged in as the user they are trying to save as. 
-    Xerxes_Helper::ensureSpecifiedUser($strUsername, $objRequest, $objRegistry, "You must be logged in as $strUsername to save to a personal database collection owned by that user.");
+
     
     $objData = new Xerxes_DataMap();
     
