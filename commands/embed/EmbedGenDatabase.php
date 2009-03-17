e2220dd87c19248839543bf78ba173d0f7b7e8a9
dwalker <dwalker@calstate.edu>
2009-03-17 21:34:00 +0000

    clean-up for embed commands, moving objRequest and objRegistry variables to this->request and this->registry

--- commands/embed/EmbedGenDatabase.php
+++ commands/embed/EmbedGenDatabase.php
@@ -1,35 +1,24 @@
-<?php	
-	
-  
-	/**
-	 * Little helper controller for the embed function
-	 * 
-	 */
-	
-	class Xerxes_Command_EmbedGenDatabase extends Xerxes_Command_Embed
+<?php
+
+/**
+ * Little helper controller for the embed function
+ * 
+ */
+
+class Xerxes_Command_EmbedGenDatabase extends Xerxes_Command_Embed
+{
+	public function doExecute()
 	{
-    
-    
-		/**
-		 *
-		 * @param Xerxes_Framework_Request $objRequest
-		 * @param Xerxes_Framework_Registry $objRegistry
-		 * @return int status
-		 */
+		// define url params for display action, and call helper, that
+		// does all the real work. 
 		
-		public function doExecute( Xerxes_Framework_Request $objRequest, Xerxes_Framework_Registry $objRegistry )
-		{
-       //Define url params for display action, and call helper, that
-       // does all the real work. 
-       $embed_url_params = array( "base" => "embed",
-                           "action" => "database",
-                           "id" => $objRequest->getProperty("id"));
-       $direct_url_params = array("base" => "databases",
-                                  "action" => "database",
-                                  "id" => $objRequest->getProperty("id"));
-       //The meet of it in this helper method in superclass!
-       $this->doExecuteHelper($objRequest, $objRegistry, $embed_url_params, $direct_url_params);
-       return 1;
-		}
-	}	
+		$embed_url_params = array ("base" => "embed", "action" => "database", "id" => $this->request->getProperty( "id" ) );
+		$direct_url_params = array ("base" => "databases", "action" => "database", "id" => $this->request->getProperty( "id" ) );
+		
+		// the meet of it in this helper method in superclass!
+		
+		$this->doExecuteHelper( $embed_url_params, $direct_url_params );
+		return 1;
+	}
+}
 ?>
--- commands/embed/EmbedGenSubject.php
+++ commands/embed/EmbedGenSubject.php
@@ -1,37 +1,26 @@
-<?php	
-	
-  
-	/**
-	 * Little helper controller for the embed function
-	 * 
-	 */
-	
-	class Xerxes_Command_EmbedGenSubject extends Xerxes_Command_Embed
+<?php
+
+/**
+ * Little helper controller for the embed function
+ * 
+ */
+
+class Xerxes_Command_EmbedGenSubject extends Xerxes_Command_Embed
+{
+	public function doExecute()
 	{
-    
-    
-		/**
-		 *
-		 * @param Xerxes_Framework_Request $objRequest
-		 * @param Xerxes_Framework_Registry $objRegistry
-		 * @return int status
-		 */
+		// define url params for subject display action, and call helper
 		
-		public function doExecute( Xerxes_Framework_Request $objRequest, Xerxes_Framework_Registry $objRegistry )
-		{
-       //Define url params for subject display action, and call helper.
-       $embed_url_params = array( "base" => "embed",
-                           "action" => "subject",
-                           "subject" => $objRequest->getProperty("subject"));
-       
-       //Direct to resource without embed
-       $direct_url_params = array("base" => "databases",
-                                  "action" => "subject",
-                                  "subject" => $objRequest->getProperty("subject"));
-       
-       //The meet of it in this helper method in superclass!
-       $this->doExecuteHelper($objRequest, $objRegistry, $embed_url_params, $direct_url_params);
-       return 1;
-		}
-	}	
+		$embed_url_params = array ("base" => "embed", "action" => "subject", "subject" => $this->request->getProperty( "subject" ) );
+		
+		// direct to resource without embed
+		
+		$direct_url_params = array ("base" => "databases", "action" => "subject", "subject" => $this->request->getProperty( "subject" ) );
+		
+		// the meet of it in this helper method in superclass!
+		
+		$this->doExecuteHelper( $embed_url_params, $direct_url_params );
+		return 1;
+	}
+}
 ?>
