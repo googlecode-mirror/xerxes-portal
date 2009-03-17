955d69cc77c3fd9f0b110d6598c90a53b85a9d86
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2009-03-17 21:34:00 +0000

    clean-up for embed commands, moving objRequest and objRegistry variables to this->request and this->registry

--- trunk/commands/embed/EmbedGenUserCreatedCategory.php
+++ trunk/commands/embed/EmbedGenUserCreatedCategory.php
@@ -1,47 +1,35 @@
-<?php	
-	
-  
-	/**
-	 * Little helper controller for the embed function
-	 * 
-	 */
-	
-	class Xerxes_Command_EmbedGenUserCreatedCategory extends Xerxes_Command_Embed
+<?php
+
+/**
+ * Little helper controller for the embed function
+ * 
+ */
+
+class Xerxes_Command_EmbedGenUserCreatedCategory extends Xerxes_Command_Embed
+{
+	public function doExecute()
 	{
-    
-    
-		/**
-		 *
-		 * @param Xerxes_Framework_Request $objRequest
-		 * @param Xerxes_Framework_Registry $objRegistry
-		 * @return int status
-		 */
+		// we can only embed published collections, private ones are not
+		// embeddable for so many reasons. 
 		
-		public function doExecute( Xerxes_Framework_Request $objRequest, Xerxes_Framework_Registry $objRegistry )
+		$strPublished = $this->request->getData( '/*/category/@published' );
+		if ( $strPublished != '1' )
 		{
-      
-      //We can only embed published collections, private ones are not
-      //embeddable for so many reasons. 
-      $strPublished  = $objRequest->getData('/*/category/@published');
-      if ( $strPublished != '1') {        
-        throw new Xerxes_AccessDeniedException("Your collection must be published in order to use the 'embed' feature");
-      }
-      
-       //Define url params for subject display action, and call helper.
-       $embed_url_params = array( "base" => "collections",
-                           "action" => "embed",
-                           "username" => $objRequest->getProperty("username"),
-                           "subject" => $objRequest->getProperty("subject"));
-       
-       //Direct to resource without embed
-       $direct_url_params = array("base" => "collections",
-                                  "action" => "subject",
-                                  "username" => $objRequest->getProperty("username"),
-                                  "subject" => $objRequest->getProperty("subject"));
-       
-       //The meet of it in this helper method in superclass!
-       $this->doExecuteHelper($objRequest, $objRegistry, $embed_url_params, $direct_url_params);
-       return 1;
+			throw new Xerxes_AccessDeniedException( "Your collection must be published in order to use the 'embed' feature" );
 		}
-	}	
+		
+		// define url params for subject display action, and call helper.
+		
+		$embed_url_params = array ("base" => "collections", "action" => "embed", "username" => $this->request->getProperty( "username" ), "subject" => $this->request->getProperty( "subject" ) );
+		
+		// direct to resource without embed
+		
+		$direct_url_params = array ("base" => "collections", "action" => "subject", "username" => $this->request->getProperty( "username" ), "subject" => $this->request->getProperty( "subject" ) );
+		
+		// the meet of it in this helper method in superclass!
+		
+		$this->doExecuteHelper( $embed_url_params, $direct_url_params );
+		return 1;
+	}
+}
 ?>
