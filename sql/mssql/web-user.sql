569affa95f5d622e4782ece1c96a87bd19df2214
dwalker <dwalker@calstate.edu>
2009-06-09 15:45:33 +0000

    generic password for webuser

--- sql/mssql/web-user.sql
+++ sql/mssql/web-user.sql
@@ -1,5 +1,5 @@
 
 USE xerxes;
-CREATE LOGIN xerxes WITH PASSWORD = 'Cyru$';
+CREATE LOGIN xerxes WITH PASSWORD = 'password';
 CREATE user xerxes FOR LOGIN xerxes;
 GRANT SELECT, INSERT, DELETE, UPDATE TO xerxes;
