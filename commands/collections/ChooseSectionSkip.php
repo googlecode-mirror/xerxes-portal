c2592a841e48190c90bd98ecdda1c9799163002d
rochkind <rochkind@jhu.edu>
2009-07-13 17:56:13 +0000

    correctly determine username for save database actions

--- commands/collections/ChooseSectionSkip.php
+++ commands/collections/ChooseSectionSkip.php
@@ -12,6 +12,9 @@ class Xerxes_Command_ChooseSectionSkip extends Xerxes_Command_Collections
 	{
 		$strSubjectSelection = $this->request->getProperty( "subject" );
 		$strUsername = $this->request->getProperty( "username" );
+    if (empty($strUsername)) {
+      $strUsername = $this->request->getSession("username");
+    }
 		
 		// make sure they are logged in as the user they are trying to save as
 		
