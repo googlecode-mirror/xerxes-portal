aed3aeb6c5684939a1771a9e3c86f2839139d11a
dwalker <dwalker@calstate.edu>
2009-08-21 21:03:36 +0000

    added support for variably-named local custom authentication scheme

--- commands/authenticate/Authenticate.php
+++ commands/authenticate/Authenticate.php
@@ -27,45 +27,98 @@
 				}
 			}
 			
-			// now make the object
+			// make sure it's in our list, or if blank still, we get the default
 			
 			$configAuth = $objRegistry->getAuthenticationSource($override);
+			
+			// now make it!
 			
 			switch ( $configAuth )
 			{
 				case "ldap":
+					
 					$this->authentication = new Xerxes_LDAP($objRequest, $objRegistry);
 					break;
 					
 				case "innovative":
-					require_once("config/authentication/innovative.php");
-					$this->authentication =  new Xerxes_InnovativePatron_Local($objRequest, $objRegistry);
+					
+					$iii_file = "config/authentication/innovative.php";
+					
+					if ( file_exists($iii_file) )
+					{
+						require_once($iii_file);
+						$this->authentication = new Xerxes_InnovativePatron_Local($objRequest, $objRegistry);
+					}
+					else
+					{
+						$this->authentication = new Xerxes_InnovativePatron($objRequest, $objRegistry);
+					}
 					break;
 					
 				case "cas":
-					$this->authentication =  new Xerxes_CAS($objRequest, $objRegistry);
+					
+					$this->authentication = new Xerxes_CAS($objRequest, $objRegistry);
 					break;
 					
 				case "guest":
-					$this->authentication =  new Xerxes_GuestAuthentication($objRequest, $objRegistry);
+					
+					$this->authentication = new Xerxes_GuestAuthentication($objRequest, $objRegistry);
 					break;					
 
 				case "demo":
-					$this->authentication =  new Xerxes_DemoAuthentication($objRequest, $objRegistry);
+					
+					$this->authentication = new Xerxes_DemoAuthentication($objRequest, $objRegistry);
 					break;		
 
 				case "shibboleth":
-					require_once("config/authentication/shibboleth.php");
-					$this->authentication =  new Xerxes_Shibboleth_Local($objRequest, $objRegistry);
+					
+					$shib_file = "config/authentication/shibboleth.php";
+
+					if ( file_exists($shib_file) )
+					{
+						require_once($shib_file);
+						$this->authentication = new Xerxes_Shibboleth_Local($objRequest, $objRegistry);
+					}
+					else
+					{
+						$this->authentication = new Xerxes_Shibboleth($objRequest, $objRegistry);
+					}
 					break;			
 				
 				case "custom":
+					
 					require_once("config/authentication/custom.php");
 					$this->authentication =  new Xerxes_CustomAuth($objRequest, $objRegistry);
 					break;
 					
 				default:
-					throw new Exception("unsupported authentication type");
+					
+					// check to see if a file exists with an authentication class that extends the framework,
+					// if so, then use it; this supports multiple custom schemes
+					
+					$local_file = "config/authentication/$configAuth.php";
+					$class_name = "Xerxes_CustomAuth_" . strtoupper( substr( $configAuth, 0, 1 ) ) . substr( $configAuth, 1 );
+					
+					if ( file_exists($local_file) )
+					{
+						require_once($local_file);
+						
+						if ( ! class_exists($class_name) )
+						{
+							throw new Exception("the custom authentication scheme '$configAuth' should have a class called '$class_name'");
+						}
+						
+						$this->authentication =  new $class_name($objRequest, $objRegistry);
+						
+						if ( ! $this->authentication instanceof Xerxes_Framework_Authenticate)
+						{
+							throw new Exception("class '$class_name' for the '$configAuth' authentication scheme must extend Xerxes_Framework_Authenticate");
+						}
+					}
+					else
+					{
+						throw new Exception("unsupported authentication type");
+					}
 			}
 			
 			// we set this so we can keep track of the authentication type
