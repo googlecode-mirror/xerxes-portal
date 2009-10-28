9778683448c504a8c3423b1cdaf24bc6c6188463
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2009-10-28 20:26:28 +0000

    search arch, take 2

---
+++ branches/search-arch/lib/Xerxes/GuestAuthentication.php
@@ -0,0 +1,30 @@
+<?php
+
+	/**
+	 * Guest Authentication
+	 * 
+	 * @author David Walker
+	 * @copyright 2008 California State University
+	 * @version $Id$
+	 * @package Xerxes
+	 * @link http://xerxes.calstate.edu
+	 * @license http://www.gnu.org/licenses/
+	 */
+
+	class Xerxes_GuestAuthentication extends Xerxes_Framework_Authenticate 
+	{
+		/**
+		 * Just register the user with a role of guest
+		 */
+		
+		public function onLogin()
+		{
+			$this->role = "guest";
+			$this->user->username = "guest@" . session_id();
+			$this->register();
+				
+			return true;
+		}
+	}
+
+?>
