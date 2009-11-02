d803c3a79a326b8e942cef23b96fbb9adc47b1b5
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2009-11-02 23:03:39 +0000

    1.6 branch

---
+++ branches/1.6.x/commands/collections/RemoveDatabaseFromUserSubcategory.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Can be used to remove a database from a user-created subcategory (if param
+ *  "db" is present), or delete a subcategory (if just "subcategory" param). 
+ *
+ *  In all cases, you need a 'subject' and 'username' param, and 'return'. 
+ *
+ * @author Jonathan Rochkind
+ * @copyright 2009 Johns Hopkins University
+ * @link http://xerxes.calstate.edu
+ * @license http://www.gnu.org/licenses/
+ * @version $Id$
+ * @package Xerxes
+ */
+
+class Xerxes_Command_RemoveDatabaseFromUserSubcategory extends Xerxes_Command_Collections
+{
+	public function doExecute()
+	{
+		$strNormalizedSubject = $this->request->getProperty( "subject" );
+		$strUsername = $this->request->getProperty( "username" );
+		$strDatabaseID = $this->request->getProperty( "id" );
+		
+		$strSubcatID = $this->request->getProperty( "subcategory" );
+		
+		// Make sure they are logged in as the user they are trying to save as. 
+		Xerxes_Helper::ensureSpecifiedUser( $strUsername, $this->request, $this->registry, "You must be logged in as $strUsername to save to a personal database collection owned by that user." );
+		
+		$objData = new Xerxes_DataMap( );
+		$subcategory = null;
+		
+		// Find the category
+		
+		$category = $objData->getSubject( $strNormalizedSubject, null, Xerxes_DataMap::userCreatedMode, $strUsername );
+		$subcategory = $this->getSubcategory( $category, $strSubcatID );
+		
+		$objData->removeDatabaseFromUserCreatedSubcategory( $strDatabaseID, $subcategory );
+		
+		// Send them back where they came from, with a message. 
+		$this->returnWithMessage( "Removed database" );
+		
+		return 1;
+	}
+}
+?>
