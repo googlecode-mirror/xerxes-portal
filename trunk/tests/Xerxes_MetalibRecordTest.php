302003889cc0e58e53eba04d181e75298b6548b4
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2010-01-19 22:01:38 +0000

    updating tests for new parser

--- trunk/tests/Xerxes_MetalibRecordTest.php
+++ trunk/tests/Xerxes_MetalibRecordTest.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../lib/framework/Parser.php';
 require_once '../lib/Xerxes/Marc.php';
 require_once '../lib/Xerxes/Record.php';
 require_once '../lib/Xerxes/metalib/MetalibRecord.php';
@@ -71,6 +72,8 @@ class Xerxes_MetalibRecordTest extends PHPUnit_Framework_TestCase
 		$record = $this->Xerxes_MetalibRecord_Document->record(1);
 		
 		$this->assertEquals( $record->getDOI(), "10.1017/S0144686X08007940");
+		
+		echo $record->getOpenURL();
 		
 		// test author 100 -> 700 mapping
 		
