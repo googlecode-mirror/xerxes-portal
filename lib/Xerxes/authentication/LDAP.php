b3d71a4bcb74f9928566df7f412ca99c51aaca9a
dwalker <dwalker@calstate.edu>
2010-07-07 18:33:53 +0000

    new ldap options

--- lib/Xerxes/authentication/LDAP.php
+++ lib/Xerxes/authentication/LDAP.php
@@ -19,13 +19,38 @@
 		
 		public function onCallBack()
 		{
-			$strUsername = $this->request->getProperty( "username" );
-			$strPassword = $this->request->getProperty( "password" );
+			$strUsername		= $this->request->getProperty( "username" );
+			$strPassword		= $this->request->getProperty( "password" );
 			
-			$strController = $this->registry->getConfig( "DIRECTORY_SERVER", true );
-			$strDomain = $this->registry->getConfig( "DOMAIN", true );
+			$strController		= $this->registry->getConfig( "DIRECTORY_SERVER", true );
+			$strDomain			= $this->registry->getConfig( "DOMAIN", false );
+			
+			// backwards compatability with now deprecated domain entry
+			
+			$strDNFormat = "";
+			
+			if ( $strDomain != "" )
+			{
+				$strDNFormat = "%s@$strDomain";
+			}
+			else
+			{
+				$strDNFormat	= $this->registry->getConfig( "LDAP_DN_FORMAT", true );
+			}
+			
+			$bolDoInitBind		= $this->registry->getConfig( "LDAP_DO_INIT_BIND", false, false );
+			$strInitBindDN		= $this->registry->getConfig( "LDAP_INIT_BIND_DN", false );
+			$strInitBindPwd		= $this->registry->getConfig( "LDAP_INIT_BIND_PASSWORD", false );
+			$strSearchBase		= $this->registry->getConfig( "LDAP_SEARCH_BASE", false );
+			$strSearchFilter	= $this->registry->getConfig( "LDAP_SEARCH_FILTER", false );
+			$strSearchUid		= $this->registry->getConfig( "LDAP_SEARCH_UID", false );
+			$strSearchName		= strtolower($this->registry->getConfig( "LDAP_SEARCH_NAME", false ));
+			$strSearchSurname	= strtolower($this->registry->getConfig( "LDAP_SEARCH_SURNAME", false ));
+			$strSearchMail		= strtolower($this->registry->getConfig( "LDAP_SEARCH_MAIL", false ));
 			
 			$bolAuth = false;
+			
+
 			
 			// connect to ldap server
 			
@@ -35,26 +60,82 @@
 			{
 				if ( $strPassword != null )
 				{
-					// bind with username and pass
+					if ( $bolDoInitBind != true )
+					{
+						// try to bind directly using provided username / password
+
+						// construct the user DN from username
+						$strBindDN = sprintf($strDNFormat, $strUsername);
+					}
+					else
+					{
+						// first, do the initial bind
+						// if $strInitBindDN and $strInitBindPwd are both empty, do anonymous bind
+
+						$bolInitBind = ldap_bind($objConn, $strInitBindDN, $strInitBindPwd);
+						
+						if ( $bolInitBind )
+						{
+							// search for the user DN in the directory tree
+
+							$arrFields = array($strSearchUid);
+							$strSearchFilter2 = sprintf($strSearchFilter, $strUsername);
+							$objResults = @ldap_search($objConn, $strSearchBase, $strSearchFilter2, $arrFields, 0, 3);
+							
+							if ( $objResults ) 
+							{
+								$objEntries = ldap_get_entries($objConn, $objResults);
+								
+								if ($objEntries['count'] == 0)	
+								{
+									$strBindDN = 'USER_NOT_FOUND';
+								}
+								else
+								{
+									$strBindDN = $objEntries[0]['dn'];
+								}
+							}
+						}
+					}
+
+					// try to bind using user DN
+					if ($strBindDN != 'USER_NOT_FOUND')
+					{
+						$bolAuth = ldap_bind($objConn, $strBindDN, $strPassword);
+
+						// search again (in case we didn't do the initial bind) to retrieve name, surname and email
+
+						if (!empty($strSearchName) && !empty($strSearchName) and !empty($strSearchName)) 
+						{
+							$arrFields = array($strSearchUid, $strSearchSurname, $strSearchName);
+							$strSearchFilter2 = sprintf($strSearchFilter, $strUsername);
+							$objResults = @ldap_search($objConn, $strSearchBase, $strSearchFilter2, $arrFields, 0, 3);
+							
+							if ( $objResults ) 
+							{
+								$objEntries = ldap_get_entries($objConn, $objResults);
+								
+								if (isset($objEntries[0][$strSearchName][0]))
+									$this->user->first_name = $objEntries[0][$strSearchName][0];
+								if (isset($objEntries[0][$strSearchSurname][0]))
+									$this->user->last_name  = $objEntries[0][$strSearchSurname][0];
+								if (isset($objEntries[0][$strSearchMail][0]))
+									$this->user->email_addr = $objEntries[0][$strSearchMail][0];
+							}
+						}
+					}
+					ldap_close($objConn);
+				}
+
+				if ( $bolAuth == true )
+				{
+					// register the user and stop the flow
 					
-					$bolAuth = ldap_bind($objConn, $strUsername . "@" . $strDomain, $strPassword);
-				} 
-				
-				ldap_close($objConn);
+					$this->user->username = $strUsername;
+					$this->register();
+				}
 			}
-					
-			if ( $bolAuth == true )
-			{
-				// register the user and stop the flow
-				
-				$this->user->username = $strUsername;
-				$this->register();
-				return true;
-			}
-			else
-			{
-				return false;
-			}
+			return $bolAuth;
 		}
 	}
 
