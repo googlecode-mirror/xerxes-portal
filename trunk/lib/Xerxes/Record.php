239fa4a0d43bfa1c4bb7e6a4a5f1bb844a4abdf3
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2010-09-30 16:49:35 +0000

    author can be list as well

--- trunk/lib/Xerxes/Record.php
+++ trunk/lib/Xerxes/Record.php
@@ -2475,7 +2475,7 @@ class Xerxes_Record extends Xerxes_Marc_Record
 		
 		// author can be string or data field
 		
-		if ($author instanceof Xerxes_Marc_DataField )
+		if ($author instanceof Xerxes_Marc_DataField || $author instanceof Xerxes_Marc_DataFieldList)
 		{
 			$strAuthor = $author->subfield($subfields);
 			$strAuthorDisplay = $author->__toString();
