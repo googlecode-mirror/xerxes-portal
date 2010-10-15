26dd09f5b9e8f0f0843df754e9bbbf2013ce22ae
dwalker <dwalker@calstate.edu>
2010-10-15 18:53:36 +0000

    common date facet functionality for lucene-based systems (Solr, Primo Cent)

--- lib/framework/Search.php
+++ lib/framework/Search.php
@@ -1390,7 +1390,132 @@ abstract class Xerxes_Framework_Search
 	protected function extractFacets(DOMDocument $xml)
 	{
 		
-	}	
+	}
+	
+	
+	protected function luceneDateToDecade($facet_array, $bottom_decade = 1900)
+	{
+		// ksort($facet_array); print_r($facet_array);
+		
+		$bottom_year = $bottom_decade - 1; // the year before the bottom decade
+		
+		$decades = array();
+		$decade_display = array();
+		
+		$top = date("Y"); // keep track of top most year
+		$bottom = $bottom_year; // and the bottom most year
+		$top_of_bottom = 0; // the top most of the bottom group
+		
+		foreach ( $facet_array as $year => $value)
+		{
+			// set a new top 
+			
+			if ( $year > $top )
+			{
+				$top = $year;			
+			}
+			
+			// strip the end year, getting just century and decade
+			
+			$dec = substr($year,0,3);
+			
+			// if the end date in this decade is beyond the current year, then
+			// we are in the current decade
+			
+			$dec_end = (int) $dec . "9";
+							
+			if ( $dec_end > date("Y") )
+			{
+				$display = $dec . "0-present";
+			}
+			else
+			{
+				// otherwise we're going DDD0-D9
+				
+				$display = $dec . "0-" . substr($dec,2,1) . "9";
+			}
+			
+			// but the actual query is the dates themselves
+							
+			$query = "[" . $dec . "0 TO " . $dec . "9]";
+			
+			// for the old stuff, just group it together
+			
+			$bottom_decade = $bottom_decade - 1;
+			
+			if ( $year <= $bottom_year )
+			{
+				// set a new bottom for display purposes
+				
+				if ( $year < $bottom )
+				{
+					$bottom = $year;
+				}
+
+				// and the top of the bottom group
+				
+				if ( $year > $top_of_bottom )
+				{
+					$top_of_bottom = $year;
+				}				
+				
+				$query = "[-999999999 TO $bottom_year]";
+				$display = "before-$bottom_year";
+			}
+			
+			$decade = array();
+			$decade["display"] = $display;
+			$decade["query"] = $query;
+		
+			$query = $decade["query"];
+						
+			$decade_display[$query] = $decade["display"];
+					
+			if ( array_key_exists($query, $decades) )
+			{
+				$decades[$query] += (int) $value; 
+			}
+			else
+			{
+				$decades[$query] = (int) $value; 
+			}
+		}
+		
+		// now replace the 'present' and 'bottom' place holders 
+		// with actual top and bottom year values
+		
+		foreach ( $decade_display as $key => $value )
+		{
+			if ( strstr($value,"present") )
+			{
+				$decade_display[$key] = str_replace("present", $top, $value);
+			}
+			if ( strstr($value,"before") )
+			{
+				$decade_display[$key] = str_replace("before", $bottom, $value);
+			}
+			
+			// now eliminate same year scenario
+			
+			$date = explode("-", $decade_display[$key]);
+			
+			if ( $date[0] == $date[1] )
+			{
+				$decade_display[$key] = $date[0];
+			}
+			
+		}
+		
+		// sort em in date order
+		
+		krsort($decades);
+		
+		$final = array();
+		$final["decades"] = $decades;
+		$final["display"] = $decade_display;
+		
+		return $final;
+	}
 	
 	/**
 	 * Extract all the ISSNs from the records, convenience funciton
@@ -1752,6 +1877,12 @@ class Xerxes_Framework_Search_Engine
 		return $this->url;
 	}
 	
+	public function hits($search)
+	{
+		$this->searchRetrieve($search,0,0);
+		return $this->total;
+	}
+	
 	public function getTotal()
 	{
 		return $this->total;	
@@ -1919,6 +2050,18 @@ class Xerxes_Framework_Search_Config extends Xerxes_Framework_Registry
 	{
 		$facet = $this->getFacet($internal);
 		return (string) $facet["type"];
+	}
+	
+	public function isDateType($internal)
+	{
+		if ( $this->getFacetType($internal) == "date" )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	public function getFacet($internal)
