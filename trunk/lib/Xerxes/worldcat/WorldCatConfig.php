db7490dc862618b06a6af96b33fa870bfd2dbf10
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2010-11-02 12:20:30 +0000

    but don't include .xml in path

--- trunk/lib/Xerxes/worldcat/WorldCatConfig.php
+++ trunk/lib/Xerxes/worldcat/WorldCatConfig.php
@@ -13,7 +13,7 @@
 
 class Xerxes_WorldCatConfig extends Xerxes_Framework_Registry
 {
-	protected $config_file = "config/worldcat.xml";
+	protected $config_file = "config/worldcat";
 	private static $instance; // singleton pattern
 	
 	public static function getInstance()
