30d4f729e75ea019ea76009043e846d79681018d
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2010-11-16 23:30:33 +0000

    switching to detectmobilebrowsers.mobi mobile detect function

--- trunk/lib/framework/Request.php
+++ trunk/lib/framework/Request.php
@@ -186,70 +186,13 @@ class Xerxes_Framework_Request
 	
 	/**
 	 * Simple function to detect if the user has a mobile device
-	 * http://www.russellbeattie.com/blog/mobile-browser-detection-in-php
 	 */
 	
 	public function isMobileDevice()
 	{
-		$isMobile = false;
-		
-		$op = strtolower($this->getServer('HTTP_X_OPERAMINI_PHONE'));
-		$ua = strtolower($this->getServer('HTTP_USER_AGENT'));
-		$ac = strtolower($this->getServer('HTTP_ACCEPT'));
-		
-		// custom ipad check
-
-		if (strpos($ua, 'ipad')) return false;
-		
-		// others
-		
-		$isMobile = strpos($ac, 'application/vnd.wap.xhtml+xml') !== false
-			|| $op != ''
-			|| strpos($ua, 'sony') !== false 
-			|| strpos($ua, 'symbian') !== false 
-			|| strpos($ua, 'nokia') !== false 
-			|| strpos($ua, 'samsung') !== false 
-			|| strpos($ua, 'mobile') !== false
-			|| strpos($ua, 'windows ce') !== false
-			|| strpos($ua, 'epoc') !== false
-			|| strpos($ua, 'opera mini') !== false
-			|| strpos($ua, 'nitro') !== false
-			|| strpos($ua, 'j2me') !== false
-			|| strpos($ua, 'midp-') !== false
-			|| strpos($ua, 'cldc-') !== false
-			|| strpos($ua, 'netfront') !== false
-			|| strpos($ua, 'mot') !== false
-			|| strpos($ua, 'up.browser') !== false
-			|| strpos($ua, 'up.link') !== false
-			|| strpos($ua, 'audiovox') !== false
-			|| strpos($ua, 'blackberry') !== false
-			|| strpos($ua, 'ericsson,') !== false
-			|| strpos($ua, 'panasonic') !== false
-			|| strpos($ua, 'philips') !== false
-			|| strpos($ua, 'sanyo') !== false
-			|| strpos($ua, 'sharp') !== false
-			|| strpos($ua, 'sie-') !== false
-			|| strpos($ua, 'portalmmm') !== false
-			|| strpos($ua, 'blazer') !== false
-			|| strpos($ua, 'avantgo') !== false
-			|| strpos($ua, 'danger') !== false
-			|| strpos($ua, 'palm') !== false
-			|| strpos($ua, 'series60') !== false
-			|| strpos($ua, 'palmsource') !== false
-			|| strpos($ua, 'pocketpc') !== false
-			|| strpos($ua, 'smartphone') !== false
-			|| strpos($ua, 'rover') !== false
-			|| strpos($ua, 'ipaq') !== false
-			|| strpos($ua, 'au-mic,') !== false
-			|| strpos($ua, 'alcatel') !== false
-			|| strpos($ua, 'ericy') !== false
-			|| strpos($ua, 'up.link') !== false
-			|| strpos($ua, 'vodafone/') !== false
-			|| strpos($ua, 'wap1.') !== false
-			|| strpos($ua, 'wap2.') !== false
-			|| strpos($ua, 'maemo browser') !== false;
-			
-		return $isMobile;		
+		require_once(Xerxes_Framework_FrontController::parentDirectory() . '/lib/mobile/mobile_device_detect.php');		
+		$is_mobile = mobile_device_detect(true, false);
+		return $is_mobile[0];
 	}
 
 	/**
