32edc32dcc16e09c456e25f25def061b87dce108
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2010-12-07 16:25:48 +0000

    new Xerxes_Framework_Labels class, and making sure chosen language is accessed via Request object, not Registry

--- trunk/commands/helper/HelperLabels.php
+++ trunk/commands/helper/HelperLabels.php
@@ -15,41 +15,8 @@ class Xerxes_Command_HelperLabels extends Xerxes_Command_Helper
 {
 	public function doExecute()
 	{
-		$parent = $this->registry->getConfig("PATH_PARENT_DIRECTORY");
-		
-		$xml = new DOMDocument();
-		$xml->load("$parent/lib/xsl/labels/eng.xsl");
-		
-		if ( file_exists("xsl/labels/eng.xsl") )
-		{
-			$local_xml = new DOMDocument();
-			$local_xml->load("xsl/labels/eng.xsl");
-			$import = $xml->importNode($local_xml->documentElement, true);
-			$xml->documentElement->appendChild($import);			
-		}
-		
-		// if language is set to something other than english
-		// then include that file to override the english labels
-
-		$language = $this->registry->getConfig("LANGUAGE");
-		
-		if ( $language != "" )
-		{
-			$language_xml = new DOMDocument();
-			$language_xml->load("$parent/lib/xsl/labels/$language.xsl");
-			
-			$import = $xml->importNode($language_xml->documentElement, true);
-			$xml->documentElement->appendChild($import);
-
-			if ( file_exists("xsl/labels/$language.xsl") )
-			{
-				$local_xml = new DOMDocument();
-				$local_xml->load("xsl/labels/$language.xsl");
-				$import = $xml->importNode($local_xml->documentElement, true);
-				$xml->documentElement->appendChild($import);			
-			}		
-		}	
-		
+		$labels = Xerxes_Framework_Labels::getInstance();
+		$xml = $labels->getXML();
 		$this->request->addDocument($xml);
 	}
 }
