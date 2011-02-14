2237acaaceac8b8db45cd456e8adb623f9dbc5e0
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2011-02-14 23:27:09 +0000

    [x2] adding 1.x framework, plus Response object

---
+++ branches/2.x/application/lib/framework/Error.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * Displays errors and performs configued actions for uncaught exceptions
+ *
+ * @author David Walker
+ * @copyright 2008 California State University
+ * @version $Id: Error.php 1563 2010-12-13 18:08:57Z dwalker@calstate.edu $
+ * @package  Xerxes_Framework
+ * @link http://xerxes.calstate.edu
+ * @license http://www.gnu.org/licenses/
+ */
+
+class Xerxes_Framework_Error
+{
+	/**
+	 * Do something with uncaught errors
+	 */
+	
+	public static function handle($e, Xerxes_Framework_Request $objRequest, Xerxes_Framework_Registry $objRegistry)
+	{
+		if ( $objRegistry->getConfig( "DISPLAY_ERRORS", false, false ) )
+		{
+			throw $e;
+		}
+		
+		// flag certain exception types for special handling in the xslt
+
+		$strErrorType = get_class( $e );
+		
+		// might be a sub-class, reset so view will catch. 
+
+		if ( $e instanceof PDOException )
+		{
+			$strErrorType = "PDOException";
+		}
+		
+		// if this is the command line, just rethrow the error so we can see it; might
+		// make this a little better formatted in the future
+
+		if ( $objRequest->isCommandLine() )
+		{
+			throw $e;
+		} 
+		else
+		{
+			// translate heading and message
+			
+			$labels = Xerxes_Framework_Labels::getInstance();
+
+			if ( $e instanceof Xerxes_Exception )
+			{
+				$heading = $e->heading();
+			}			
+			else
+			{
+				$heading = "text_error";
+			}
+			
+			$heading = $labels->getLabel($heading);
+			$message = $labels->getLabel($e->getMessage());
+			
+			// first output to apache error log
+			
+			error_log( "Xerxes error: " . $message . ": " . $e->getTraceAsString() );
+			
+			//set proper http response code
+
+			$resultStatus = 500;
+			
+			if ( $e instanceof Xerxes_Exception_AccessDenied )
+			{
+				$resultStatus = 403;
+			} 
+			else if ( $e instanceof Xerxes_Exception_NotFound )
+			{
+				$resultStatus = 404;
+			}
+			
+			header( ' ', true, $resultStatus ); // send back http status as internal server error or other specified status
+			
+			// for the web, we'll convert the error message to xml along with the type
+			// of exception and hand display off to the error.xsl file
+
+			$objError = new DOMDocument( );
+			
+			$objError->loadXML( "<error />" );
+
+			$objMessage = $objError->createElement( "message", $message );
+			$objMessage->setAttribute( "type", $strErrorType );
+			$objError->documentElement->appendChild( $objMessage );
+			
+			$objHeading = $objError->createElement( "heading", $heading );
+			$objError->documentElement->appendChild( $objHeading );
+			
+			// make sure we're showing the main error file
+			
+			$objRegistry->setConfig("XSL_PARENT_DIRECTORY", null);
+			
+			
+			
+			// set the base url for the error.xsl file's benefit; don't want to assume that 
+			// the earlier code to this effect was executed before an exception, so this is redundant
+			
+			$base_path = $objRegistry->getConfig( 'BASE_WEB_PATH', false, "" );
+			$this_server_name = $objRequest->getServer( 'SERVER_NAME' );
+			
+			// check for a non-standard port
+						
+			$port = $objRequest->getServer( 'SERVER_PORT' );
+			
+			if ( $port == 80 || $port == 443 )
+			{
+			    $port = "";
+			}
+			else
+			{
+			    $port = ":" . $port;
+			}
+			
+			$protocol = "http://";
+			
+			if ( $objRequest->getServer("HTTPS") )
+			{
+				$protocol = "https://";
+			}
+			
+			$web = $protocol . $this_server_name . $port . $base_path;
+			
+			$objBaseURL = $objError->createElement( "base_url", $web );
+			$objError->documentElement->appendChild( $objBaseURL );
+			
+			// if it's a db denied exception, include info on dbs. 
+
+			if ( $e instanceof Xerxes_Exception_DatabasesDenied )
+			{
+				$excluded_xml = $objError->createElement( "excluded_dbs" );
+				$objError->documentElement->appendChild( $excluded_xml );
+				foreach ( $e->deniedDatabases() as $db )
+				{
+					$element = Xerxes_Helper::databaseToNodeset( $db, $objRequest, $objRegistry );
+					$element = $objError->importNode( $element, true );
+					$excluded_xml->appendChild( $element );
+				}
+			}
+			
+			// add in the request object's stuff
+			
+
+			$request_xml = $objRequest->toXML();
+			
+			$imported = $objError->importNode( $request_xml->documentElement, true );
+			foreach ( $imported->childNodes as $childNode )
+			{
+				$objError->documentElement->appendChild( $childNode );
+			}
+			
+			if ( $objRequest->getProperty( "format" ) == "xerxes" )
+			{
+				header( 'Content-type: text/xml' );
+				echo $objError->saveXML();
+			} 
+			else
+			{
+				// display it to the user. Transform will pick up local
+				// xsl for error page too, great.
+				
+
+				echo Xerxes_Framework_Parser::transform( $objError, "xsl/error.xsl" );
+			}
+		}
+		
+	// need to incorporate methods for doing additional actions based on the type
+	// of error -- probably a config option
+	
+
+	}
+}
+
+?>
---
+++ branches/2.x/application/lib/framework/Response.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+ * Response Object
+ * 
+ * @author David Walker
+ * @copyright 2008 California State University
+ * @link http://xerxes.calstate.edu
+ * @license http://www.gnu.org/licenses/
+ * @version $Id: Request.php 1497 2010-11-16 23:30:33Z dwalker@calstate.edu $
+ * @package Xerxes_Framework
+ * @uses Xerxes_Framework_Parser
+ */
+
+class Xerxes_Framework_Response
+{
+	private $_data = array(); // data
+	private $_redirect = ""; // redirect url
+	
+	/**
+	 * Add data to the response
+	 * 
+	 * @param mixed $object		any kind of object you likes
+	 * @param string $id		an identifier for this object
+	 */
+	
+	public function add($object, $id)
+	{
+		$this->_data[$id] = $object;
+	}
+	
+	/**
+	 * Set the URL for redirect
+	 *
+	 * @param string $url
+	 */
+	
+	public function setRedirect($url)
+	{
+		$this->_redirect = $url;
+	}
+	
+	/**
+	 * Get the URL to redirect user
+	 *
+	 * @return unknown
+	 */
+	
+	public function getRedirect()
+	{
+		return $this->_redirect;
+	}
+	
+	public function get($id)
+	{
+		if ( array_key_exists($id, $this->_data) )
+		{
+			return $this->_data[$id];
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public function toXML()
+	{
+		$xml = new DOMDocument();
+		$xml->loadXML("<xerxes />");
+		
+		foreach ( $this->_data as $id => $object )
+		{
+			$this->addToXML($xml, $id, $object);
+		}
+		
+		return $xml;
+	}
+	
+	private function addToXML(DOMDocument &$xml, $id, $object)
+	{	
+		$object_xml = null;
+		
+		if ( is_int($id) )
+		{
+			$id = "object_$id";
+		}
+		
+		// no value, no mas!
+		
+		if ( $object == "" )
+		{
+			return null;
+		}
+		
+		// this object defines its own toXML method, so use that
+		
+		if ( method_exists($object, "toXML") )
+		{
+			$object_xml = $object->toXML();
+		}
+		
+		// already in xml, so take it
+		
+		elseif ( $object instanceof DOMDocument )
+		{
+			$object_xml = $object;
+		}
+		
+		// simplexml, same deal, but make it dom, yo
+		
+		elseif ( $object instanceof SimpleXMLElement )
+		{
+			$object_xml = new DOMDocument();
+			$object_xml->loadXML($object->asXML());
+		}
+		
+		// object
+		
+		elseif ( is_object($object) )
+		{
+			// this object tells us to use this id in the xml
+			
+			if ( property_exists($object, "id") )
+			{
+				$id = $object->id;
+			}
+			
+			$object_xml = new DOMDocument();
+			$object_xml->loadXML("<$id />");
+			
+			// only public properties
+			
+			$public = array_keys(get_class_vars(get_class($object)));
+			
+			foreach ( get_object_vars($object) as $property => $value )
+			{
+				if ( in_array($property, $public) )
+				{
+					$this->addToXML($object_xml, $property, $value);
+				}
+			}
+		}
+		
+		// array
+		
+		elseif ( is_array($object) ) 
+		{
+			$object_xml = new DOMDocument();
+			$object_xml->loadXML("<$id />");
+			
+			foreach ( $object as $property => $value )
+			{
+				// if the name of the array is plural, then make the childen singular
+				// if this is an array of objects, then the object may override this
+				
+				if ( is_int($property) && substr($id,-1) == "s" )
+				{
+					$property = substr($id,0,-1);
+				}
+				
+				$this->addToXML($object_xml, $property, $value);
+			}
+		}
+		
+		// assumed to be primitive type (string, bool, or int)		
+		
+		else 
+		{
+			// just create a simple new element and return this thing
+			
+			$element = $xml->createElement($id, $object);
+			$xml->documentElement->appendChild($element);
+			return $xml;
+		}
+		
+		// if we got this far, then we've got a domdocument to add
+		
+		$import = $xml->importNode($object_xml->documentElement, true);
+		$xml->documentElement->appendChild($import);			
+		
+		return $xml;
+	}
+}
+
+?>
