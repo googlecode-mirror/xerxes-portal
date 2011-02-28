de3304b0012107c765b48f1e8cf2811f747ca8a4
dwalker <dwalker@calstate.edu>
2011-02-28 20:00:15 +0000

    first outline of search and solr controller

---
+++ application/controllers/Search.php
@@ -0,0 +1,547 @@
+<?php
+
+abstract class Xerxes_Controller_Search extends Xerxes_Framework_Controller
+{
+	protected $config;
+	protected $query;
+	protected $engine;
+	
+	public function results()
+	{
+		// params
+		
+		$start = $this->request->getParam('start');
+		$max = $this->request->getParam('max');
+		$sort = $this->request->getParam('sort');
+		
+		// search
+				
+		$results = $this->engine->searchRetrieve($this->query, $start, $max, $sort);
+		
+		// add links
+		
+		$this->addRecordLinks($results);
+		$this->addFacetLinks($results);
+		
+		// response
+		
+		$this->response->add($results, "results");
+	}
+	
+	public function record()
+	{
+		$id = $this->request->getParam('id');
+		$results = $this->engine->getRecord($id);
+		
+		$this->addRecordLinks($results);
+		$this->response->add($results, "results");
+	}
+	
+	
+	######################
+	#  DISPLAY HELPERS   #
+	######################
+	
+	
+	/**
+	 * Displays paged information (e.g., 11-20 of 34 results)
+	 *
+	 * @param int $total 		total # of hits for query
+	 * @param int $start 		start value for the page
+	 * @param int $max 		maximum number of results to show
+	 *
+	 * @return array or null	summary of page results 
+	 */
+	
+	public function summary( $total, $start, $max )
+	{	
+		if ( $start == 0 )
+		{
+			$start = 1;
+		}
+			
+		// set end point
+		
+		$stop = $start + ($max - 1);
+		
+		// if end value of group of 10 exceeds total number of hits,
+		// take total number of hits as end value 
+		
+		if ( $stop > $total )
+		{
+			$stop = $total;
+		}
+		
+		if ( $total > 0 )
+		{
+			return array ( 
+				"range" => "$start-$stop",
+				"total" => number_format( $total )
+			);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	/**
+	 * Paging element
+	 * 
+	 * @param int $start
+	 * @param int $total
+	 * @param int $max
+	 * 
+	 * @return DOMDocument formatted paging navigation
+	 */
+	
+	public function pager( $start, $total, $max )
+	{
+		$objXml = new DOMDocument( );
+		$objXml->loadXML( "<pager />" );
+		
+		$base_record = 1; // starting record in any result set
+		$page_number = 1; // starting page number in any result set
+		$bolShowFirst = false; // show the first page when you get past page 10
+		
+		if ( $start == 0 ) 
+		{
+			$start = 1;
+		}
+		
+		$current_page = (($start - 1) / $max) + 1; // calculates the current selected page
+		$bottom_range = $current_page - 5; // used to show a range of pages
+		$top_range = $current_page + 5; // used to show a range of pages
+		
+		$total_pages = ceil( $total / $max ); // calculates the total number of pages
+		
+		// for pages 1-10 show just 1-10 (or whatever records per page)
+		
+		if ( $bottom_range < 5 )
+		{
+			$bottom_range = 0;
+		}
+		
+		if ( $current_page < $max )
+		{
+			$top_range = 10;
+		} 
+		else
+		{
+			$bolShowFirst = true;
+		}
+		
+		// chop the top pages as we reach the end range
+		
+		if ( $top_range > $total_pages )
+		{
+			$top_range = $total_pages;
+		}
+		
+		// see if we even need a pager
+		
+		if ( $total > $max )
+		{
+			// show first page
+			
+			if ( $bolShowFirst == true )
+			{
+				$objPage = $objXml->createElement( "page", "1" );
+				
+				$params = $this->currentParams();
+				$params["startRecord"] = 1;
+				
+				$link = $this->request->url_for( $params );
+				
+				$objPage->setAttribute( "link", Xerxes_Framework_Parser::escapeXml( $link ) );
+				$objPage->setAttribute( "type", "first" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+			
+			// create pages and links
+			
+			while ( $base_record <= $total )
+			{
+				if ( $page_number >= $bottom_range && $page_number <= $top_range )
+				{
+					if ( $current_page == $page_number )
+					{
+						$objPage = $objXml->createElement( "page", $page_number );
+						$objPage->setAttribute( "here", "true" );
+						$objXml->documentElement->appendChild( $objPage );
+					} 
+					else
+					{
+						$objPage = $objXml->createElement( "page", $page_number );
+						
+						$params = $this->currentParams();
+						$params["startRecord"] = $base_record;
+						
+						$link = $this->request->url_for( $params );
+						
+						$objPage->setAttribute( "link", Xerxes_Framework_Parser::escapeXml( $link ) );
+						$objXml->documentElement->appendChild( $objPage );
+					
+					}
+				}
+				
+				$page_number++;
+				$base_record += $max;
+			}
+			
+			$next = $start + $max;
+			
+			if ( $next <= $total )
+			{
+				$objPage = $objXml->createElement( "page", "" ); // element to hold the text_results_next label
+				
+				$params = $this->currentParams();
+				$params["startRecord"] =  $next;
+				
+				$link = $this->request->url_for( $params );
+				
+				$objPage->setAttribute( "link", Xerxes_Framework_Parser::escapeXml( $link ) );
+				$objPage->setAttribute( "type", "next" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+		}
+		
+		return $objXml;
+	}
+	
+	/**
+	 * Creates a sorting page element
+	 *
+	 * @param string $strSortQuery		initial page and querystring values
+	 * @param string $strSortKeys		selected sort value
+	 * @param array $arrOptions		list of sort options and values
+	 *
+	 * @return DOMDocument 			sort navigation
+	 */
+	
+	public function sortDisplay( $strSortQuery, $strSortKeys, $arrOptions )
+	{
+		$objXml = new DOMDocument();
+		$objXml->loadXML( "<sort_display />" );
+		
+		$strBase = "";
+		
+		if ( strstr( $strSortQuery, "?" ) )
+		{
+			$strBase = "$strSortQuery&sortKeys";
+		} 
+		else
+		{
+			$strBase = "$strSortQuery?sortKeys";
+		}
+		
+		$x = 1;
+		
+		foreach ( $arrOptions as $key => $value )
+		{
+			if ( $key == $strSortKeys )
+			{
+				$objHere = $objXml->createElement( "option", $value );
+				$objHere->setAttribute( "active", "true" );
+				$objXml->documentElement->appendChild( $objHere );
+			} 
+			else
+			{
+				$objHere = $objXml->createElement( "option", $value );
+				$objHere->setAttribute( "active", "false" );
+				$objHere->setAttribute( "link", Xerxes_Framework_Parser::escapeXml( "$strBase=$key" ) );
+				$objXml->documentElement->appendChild( $objHere );
+			}
+			
+			$x ++;
+		}
+		
+		return $objXml;
+	}
+	
+	
+	######################
+	#        LINKS       #
+	######################
+	
+	
+	/**
+	 * Add links to search results
+	 * 
+	 * @param Xerxes_Model_Search_Results $results
+	 */
+
+	protected function addRecordLinks( Xerxes_Model_Search_ResultSet &$results )
+	{	
+		// results
+				
+		foreach ( $results->getRecords() as $result )
+		{
+			$xerxes_record = $result->getXerxesRecord();
+			
+			// full-record link
+			
+			$result->url = $this->linkFullRecord($xerxes_record);
+			$result->url_full = $result->url; // backwards compatibility
+				
+			// sms link
+			
+			$result->url_sms = $this->linkSMS($xerxes_record);
+				
+			// save or delete link
+			
+			$result->url_save = $this->linkSaveRecord($xerxes_record);
+			$result->url_save_delete = $result->url_save; // backwards compatibility
+			
+			// other links
+			
+			$this->linkOther($result);
+		}
+	}
+	
+	/**
+	 * Add links to facets
+	 * 
+	 * @param Xerxes_Model_Search_Results $results
+	 */	
+	
+	protected function addFacetLinks( Xerxes_Model_Search_ResultSet &$results )
+	{	
+		// facets
+
+		$facets = $results->getFacets();
+		
+		if ( $facets != "" )
+		{
+			foreach ( $facets->getGroups() as $group )
+			{
+				foreach ( $group->getFacets() as $facet )
+				{
+					// existing url
+						
+					$url = $this->currentParams();
+							
+					// now add the new one
+							
+					if ( $facet->is_date == true ) // dates are different 
+					{
+						$url["facet.date." . $group->name . "." . 
+							urlencode($facet->key)] = $facet->name;
+					}
+					else
+					{
+						$url["facet." . $group->name] = $facet->name;									
+					}
+							
+					$facet->url = $this->request->url_for($url);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Link for spelling correction
+	 */
+	
+	protected function linkSpelling()
+	{
+		$params = $this->currentParams();
+		$params["query"] = $this->request->getProperty("spelling_query");
+		
+		return $this->request->url_for($params);
+	}
+	
+	/**
+	 * URL for the full record display
+	 * 
+	 * @param $result Xerxes_Record object
+	 * @return string url
+	 */
+	
+	protected function linkFullRecord( Xerxes_Record $result )
+	{
+		$arrParams = array(
+			"base" => $this->request->getProperty("base"),
+			"action" => "record",
+			"id" => $result->getRecordID()
+		);
+		
+		return $this->request->url_for($arrParams);
+	}
+	
+	/**
+	 * URL for the full record display
+	 * 
+	 * @param Xerxes_Record $result
+	 * @return string url
+	 */
+	
+	protected function linkSaveRecord( Xerxes_Record $result )
+	{
+		$arrParams = array(
+			"base" => $this->request->getProperty("base"),
+			"action" => "save-delete",
+			"id" => $result->getRecordID()
+		);
+		
+		return $this->request->url_for($arrParams);
+	}
+	
+	/**
+	 * URL for the sms feature
+	 * 
+	 * @param Xerxes_Record $result
+	 * @return string url
+	 */	
+	
+	protected function linkSMS(  Xerxes_Record $result )
+	{
+		$arrParams = array(
+			"base" => $this->request->getProperty("base"),
+			"action" => "sms",
+			"id" => $result->getRecordID()
+		);
+		
+		return $this->request->url_for($arrParams);	
+	}
+
+	/**
+	 * Other links for the record beyond those supplied by the framework,
+	 * such as lateral subject or author links
+	 * 
+	 * @param Xerxes_Model_Search_Result $result 
+	 */	
+	
+	protected function linkOther( Xerxes_Model_Search_Result $result )
+	{
+		return $result;
+	}
+	
+	
+	######################
+	#  PARAMS FOR LINKS  #
+	######################
+	
+	
+	/**
+	 * The current search-related parameters 
+	 * @return array
+	 */
+	
+	protected function currentParams()
+	{
+		$params = $this->query->getAllSearchParams();
+		$params["base"] = $this->request->getProperty("base");
+		$params["action"] = $this->request->getProperty("action");
+		$params["sortKeys"] = $this->request->getProperty("sortKeys");
+		
+		return $params;
+	}	
+	
+	/**
+	 * Parameters to construct the url on the search redirect
+	 * @return array
+	 */
+	
+	protected function searchRedirectParams()
+	{
+		$params = $this->currentParams();
+		$params["action"] = "results";
+		
+		return $params;
+	}
+	
+	/**
+	 * Parameters to construct the links for the paging element
+	 * @return array
+	 */
+	
+	protected function pagerLinkParams()
+	{
+		$params = $this->currentParams();
+		return $params;
+	}
+	
+	/**
+	 * Parameters to construct the links for the sort
+	 * @return array
+	 */
+	
+	protected function sortLinkParams()
+	{
+		$params = $this->currentParams();
+		
+		// remove the current sort, since we'll add the new
+		// sort explicitly to the url
+		
+		unset($params["sortKeys"]);
+		
+		return $params;
+	}
+	
+	
+	########################
+	#  SAVED RECORD STATE  #
+	########################	
+	
+	
+	/**
+	 * Store in session the fact this record is saved
+	 *
+	 * @param string $original_id		original id of the record
+	 * @param string $saved_id		the internal id in the database
+	 */ 
+	
+	protected function markSaved( $original_id, $saved_id )
+	{
+		$_SESSION['resultsSaved'][$original_id]['xerxes_record_id'] = $saved_id;
+	}
+
+	/**
+	 * Delete from session the fact this record is saved
+	 *
+	 * @param string $original_id		original id of the record
+	 */ 
+	
+	protected function unmarkSaved( $original_id )
+	{
+		if ( array_key_exists( "resultsSaved", $_SESSION ) && array_key_exists( $original_id, $_SESSION["resultsSaved"] ) )
+		{
+			unset( $_SESSION['resultsSaved'][$original_id] );
+		}
+	}
+
+	/**
+	 * Determine whether this record is already saved in session
+	 *
+	 * @param string $original_id		original id of the record
+	 */ 
+	
+	protected function isMarkedSaved($original_id)
+	{
+		if ( array_key_exists( "resultsSaved", $_SESSION ) && array_key_exists( $original_id, $_SESSION["resultsSaved"] ) )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Get the number of records saved in session
+	 */ 
+	
+	protected function numMarkedSaved()
+	{
+		$num = 0;
+		
+		if ( array_key_exists( "resultsSaved", $_SESSION ) )
+		{
+			$num = count( $_SESSION["resultsSaved"] );
+		}
+		
+		return $num;
+	}
+}
