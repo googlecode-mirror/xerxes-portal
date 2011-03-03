5ac542e318453fbb7cc81b9dad4c7d05416327db
dwalker@calstate.edu <dwalker@calstate.edu@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2011-03-03 18:06:03 +0000

    bug fix: chop long titles on save record

--- trunk/lib/Xerxes/DataMap.php
+++ trunk/lib/Xerxes/DataMap.php
@@ -1842,7 +1842,7 @@ class Xerxes_DataMap extends Xerxes_Framework_DataMap
 		$arrValues[":timestamp"] = date( "Y-m-d H:i:s" );
 		$arrValues[":username"] = $username;
 		$arrValues[":nonsort"] = $objXerxesRecord->getNonSort();
-		$arrValues[":title"] = $strTitle;
+		$arrValues[":title"] = substr($strTitle, 0, 90);
 		$arrValues[":author"] = $objXerxesRecord->getPrimaryAuthor( true );
 		$arrValues[":year"] = $iYear;
 		$arrValues[":format"] = $objXerxesRecord->getFormat();
