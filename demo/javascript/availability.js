065779801b884e124fe12500a349e3932c34a813
dwalker <dwalker@calstate.edu>
2011-03-09 18:23:07 +0000

    moving availability.js up

--- demo/javascript/availability.js
+++ demo/javascript/availability.js
@@ -5,7 +5,7 @@
  * @copyright 2010 California State University
  * @link http://xerxes.calstate.edu
  * @license http://www.gnu.org/licenses/
- * @version $Id$
+ * @version $Id: availability.js 1597 2011-01-04 19:21:15Z dwalker@calstate.edu $
  * @package Xerxes
  */
 
