618419cdd93c5f3687d3b5badee5a96a694d7df2
dwalker.calstate@gmail.com <dwalker.calstate@gmail.com@88b7fc82-5845-0410-8baa-cfedc9f4ece8>
2012-05-17 14:09:12 +0000

    google scholar export fix

--- trunk/commands/availability/PopulateFulltext.php
+++ trunk/commands/availability/PopulateFulltext.php
@@ -39,74 +39,101 @@
 					"Need config ALTERNATE_FULLTEXT_HARVEST_ADDRESS or LINK_RESOLVER_ADDRESS." );
 			}
 			
-			$strUrl = $configSfx . "/cgi/public/get_file.cgi?file=institutional_holding" . $strInstance . ".xml";
 			
 			// fire-up a transaction with the database
-			
+				
 			$objData = new Xerxes_DataMap();
 			$objData->beginTransaction();
 			
+			// clear old data
+			
 			echo "  Flushing SFX fulltext table . . . ";
-			
+				
 			$objData->clearFullText();
+				
+			echo "done.\n";			
 			
-			echo "done.\n";
-			
-			echo "  Pulling down SFX inst holdings file . . . ";
 			
 			// try to get the data from sfx
 			
-			try
+			
+			
+			$done = false;
+			$x = 0;
+			
+			while ( $done == false )
 			{
-				$strResponse = Xerxes_Framework_Parser::request($strUrl);
-				$objXml = new SimpleXMLElement($strResponse);
-			}
-			catch (Exception $e)
-			{
-				throw new Exception("cannot get institutional holding file from sfx: '$strUrl'. " .
-					"If this is the correct SFX server address, make sure your SFX allows access to " .
-					"institutional holding file from this IP address in config/get_file_restriction.config " .
-					"on SFX server.");
-			}
-			
-			echo "done.\n";
-			echo "  Processing file . . . ";
-			
-			$objItems = $objXml->xpath("//item[@type != 'other']");
-			
-			if ( $objItems == false ) throw new Exception("could not find items in inst holding file.");
-			
-			echo "done.\n";
-			echo "  Adding to database . . . ";
-			
-			foreach ( $objItems as $objItem )
-			{
-				foreach ( $objItem->coverage as $objCoverage )
+				$x++;
+				
+				$strUrl = $configSfx . "/cgi/public/get_file.cgi?file=institutional_holding" . 
+					$strInstance . '-.prt' . str_pad($x, 2, '0', STR_PAD_LEFT) . ".xml";
+				
+	
+				
+				echo "  Pulling down SFX inst holding file ($x) . . . ";
+				
+				try
 				{
-					$objFullText = new Xerxes_Data_Fulltext();
+					$strResponse = Xerxes_Framework_Parser::request($strUrl);
+					$objXml = new SimpleXMLElement($strResponse);
+				}
+				catch (Exception $e)
+				{
+					if ( $x == 1) // first time, must be error
+					{
+						throw new Exception("cannot get institutional holding file from sfx: '$strUrl'. " .
+							"If this is the correct SFX server address, make sure your SFX allows access to " .
+							"institutional holding file from this IP address in config/get_file_restriction.config " .
+							"on SFX server.");
+					}
 					
-					$objFullText->issn = (string) $objItem->issn;
-					$objFullText->issn = str_replace("-","",$objFullText->issn);
+					$done = true;
+				}
+				
+				echo "done.\n";
+				
+				if ( ! $done )
+				{
+					echo "  Processing file . . . ";
 					
-					$objFullText->title = (string) $objItem->title;
-					$objFullText->title = urlencode($objFullText->title);
-					$objFullText->title = substr(Xerxes_Framework_Parser::strtolower($objFullText->title),0, 100);
+					$objItems = $objXml->xpath("//item[@type != 'other']");
 					
-					$objFullText->startdate = (int) $objCoverage->from->year;
+					if ( $objItems == false ) throw new Exception("could not find items in inst holding file.");
 					
-					$objFullText->enddate = (int) $objCoverage->to->year;
-					if ( $objFullText->enddate == 0 ) $objFullText->enddate = 9999;
+					echo "done.\n";
+					echo "  Adding to database . . . ";
 					
-					$objFullText->embargo = (int) $objCoverage->embargo->days_not_available;
-					$objFullText->updated = date("YmdHis");
+					foreach ( $objItems as $objItem )
+					{
+						foreach ( $objItem->coverage as $objCoverage )
+						{
+							$objFullText = new Xerxes_Data_Fulltext();
+							
+							$objFullText->issn = (string) $objItem->issn;
+							$objFullText->issn = str_replace("-","",$objFullText->issn);
+							
+							$objFullText->title = (string) $objItem->title;
+							$objFullText->title = urlencode($objFullText->title);
+							$objFullText->title = substr(Xerxes_Framework_Parser::strtolower($objFullText->title),0, 100);
+							
+							$objFullText->startdate = (int) $objCoverage->from->year;
+							
+							$objFullText->enddate = (int) $objCoverage->to->year;
+							if ( $objFullText->enddate == 0 ) $objFullText->enddate = 9999;
+							
+							$objFullText->embargo = (int) $objCoverage->embargo->days_not_available;
+							$objFullText->updated = date("YmdHis");
+							
+							// add it to the database
+							
+							$objData->addFulltext($objFullText);
+						}
+					}
 					
-					// add it to the database
-					
-					$objData->addFulltext($objFullText);
+					echo "done.\n";
 				}
 			}
 			
-			echo "done.\n";
 			echo "  Commiting changes . . . ";
 			
 			$objData->commit();
